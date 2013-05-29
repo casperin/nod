4707988f99ed0e3a7e1ac9dc685c95d553c1a459
Eduardo Robles Elvira <edulix@wadobo.com>
2013-05-29 21:14:22 +0200

    Allow to check if the form is valid programatically and fixing that unchecked listeners were marking the form as with errors when there was no error

--- nod.js
+++ nod.js
@@ -266,6 +266,7 @@ Nod = (function() {
     if (!fields) {
       return;
     }
+    this.form[0].__nod = this;
     this.get = jQuery.extend({
       'delay': 700,
       'disableSubmitBtn': true,
@@ -376,6 +377,9 @@ Nod = (function() {
 
   Nod.prototype.formIsErrorFree = function() {
     return !jQuery(this.listeners).filter(function() {
+      if (this.status === null) {
+        this.runCheck();
+      }
       return !this.status;
     }).length;
   };
@@ -410,6 +414,9 @@ Nod = (function() {
 
 
 $.fn.nod = function(fields, settings) {
+  if (fields === void 0 && settings === void 0) {
+    return this[0].__nod;
+  }
   new Nod(this, fields, settings);
   return this;
 };
--- nod/init.coffee
+++ nod/init.coffee
@@ -1,3 +1,5 @@
 $.fn.nod = ( fields, settings ) ->
+  if fields == undefined && settings == undefined
+    return this[0].__nod 
   new Nod( this, fields, settings )
   return this
